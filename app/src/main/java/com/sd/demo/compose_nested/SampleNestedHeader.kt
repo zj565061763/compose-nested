42d60e69944b2c527680886aaadab45d79725afb
zjun <565061763@qq.com>
2024-01-11 10:22:57 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_nested/SampleNestedHeader.kt
+++ app/src/main/java/com/sd/demo/compose_nested/SampleNestedHeader.kt
@@ -6,16 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import com.sd.demo.compose_nested.ui.theme.AppTheme
 import com.sd.lib.compose.nested.FNestedHeader
 
@@ -51,7 +50,7 @@ private fun HeaderView(
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(300.dp)
+            .fillMaxHeight(0.5f)
             .background(Color.Red)
             .clickable {
                 logMsg { "click" }
